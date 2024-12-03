d35dd336a554d4314d88a638bda777adf74260cc
crazy-diya <dimuthulakshan2012@gmail.com>
2024-12-04 04:55:24 +0530

    first commit

--- lib/utils/app_constants.dart
+++ lib/utils/app_constants.dart
@@ -1,4 +1,3 @@
 class AppConstants {
   static const String appName = '';
-  static const String apiKey = 'fca_live_3QeGWT2q5p4gPbDsfUTyntD3BJsAPcw3A6eBYkyg';
 }
--- lib/utils/app_images.dart
+++ lib/utils/app_images.dart
@@ -1,6 +1,5 @@
 class AppImages {
   ///PNG
-  static const String currencyConvertorImage = "assets/images/pngs/cc.png";
 
   ///SVG
 
